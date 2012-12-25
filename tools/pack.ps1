39aac261ed53067b178bcab2f5497c3ccf734579
tugberkugurlu <tugberkugurlu@gmail.com>
2012-12-25 03:31:10 +0200

    polished the solution a little bit

--- tools/pack.ps1
+++
@@ -1,9 +0,0 @@
-param ([string]$solutionDir, [string]$projectPath)
-
-if((Test-Path "$solutionDir\nugetPacks") -eq $false) {
-    Write-Host "nugetPacks folder does not exists. Creating one..."
-    mkdir "$solutionDir\nugetPacks"
-}
-
-sal __Nuget "$solutionDir\tools\NuGet.exe"
-__Nuget pack $projectPath -OutputDirectory "$solutionDir\nugetPacks"
