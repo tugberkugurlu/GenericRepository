9457bedfdf48841bc4b6fd57a48841c931ba77af
tugberkugurlu <tugberkugurlu@gmail.com>
2013-06-20 15:48:25 +0300

    minor clean up

--- samples/GenericRepository.EntityFramework.SampleWebApi.Test/CountriesControllerTest.cs
+++ samples/GenericRepository.EntityFramework.SampleWebApi.Test/CountriesControllerTest.cs
@@ -5,8 +5,6 @@ using GenericRepository.EntityFramework.SampleWebApi.Test.Infrastructure;
 using Moq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Xunit;
 
 namespace GenericRepository.EntityFramework.SampleWebApi.Test {
@@ -23,7 +21,7 @@ namespace GenericRepository.EntityFramework.SampleWebApi.Test {
             
             // Arrange
 
-            var countries = new List<Country>() { 
+            List<Country> countries = new List<Country>() { 
                 new Country { Id = 1, Name = "Turkey", ISOCode = "TR", CreatedOn = DateTimeOffset.Now },
                 new Country { Id = 2, Name = "United Kingdom", ISOCode = "EN", CreatedOn = DateTimeOffset.Now },
                 new Country { Id = 3, Name = "United States", ISOCode = "US", CreatedOn = DateTimeOffset.Now },
@@ -31,17 +29,18 @@ namespace GenericRepository.EntityFramework.SampleWebApi.Test {
                 new Country { Id = 4, Name = "Bahamas", ISOCode = "BS", CreatedOn = DateTimeOffset.Now },
                 new Country { Id = 4, Name = "Uruguay", ISOCode = "UY", CreatedOn = DateTimeOffset.Now }
             };
-            var dbSet = new FakeDbSet<Country>();
+
+            FakeDbSet<Country> dbSet = new FakeDbSet<Country>();
             foreach (var country in countries) {
                 dbSet.Add(country);
             }
 
-            var entitiesContext = new Mock<IEntitiesContext>();
+            Mock<IEntitiesContext> entitiesContext = new Mock<IEntitiesContext>();
             entitiesContext.Setup(ec => ec.Set<Country>()).Returns(dbSet);
-            var countriesRepo = new EntityRepository<Country>(entitiesContext.Object);
-            var countriesController = new CountriesController(countriesRepo, Mapper.Engine);
-            var pageIndex = 1;
-            var pageSize = 3;
+            EntityRepository<Country> countriesRepo = new EntityRepository<Country>(entitiesContext.Object);
+            CountriesController countriesController = new CountriesController(countriesRepo, Mapper.Engine);
+            const int pageIndex = 1;
+            const int pageSize = 3;
 
             // Act
             var paginatedDto = countriesController.GetCountries(pageIndex, pageSize);
